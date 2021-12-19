29dd49360384a1d2a7a313aa06e885b17bf087f5
tanwei0425 <tanwei425@gmail.com>
2021-12-19 16:30:23 +0800

    1

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -5,9 +5,9 @@ const Index = () => {
         <Layouts title={'随机'} >
             <div
                 className="templates-wrapper"
-                style={{ fontSize: 60, height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                style={{ fontSize: 60, height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
             >
-                待开发
+                开发中
             </div>
         </Layouts>
     )
